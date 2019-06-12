af2dd4b62db682eb8f2e1b42a87bf750eb41c769
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-06-12 09:27:22 +0300

    commit with gitIgnore

--- App.js
+++ App.js
@@ -6,7 +6,9 @@
  * @flow
  */
 import React, {Component} from 'react';
-	import {Platform, StyleSheet, Text, View, Button, Alert, TouchableOpacity, AsyncStorage} from 'react-native';
+  import {Platform, StyleSheet, Text, View, Button, Alert, TouchableOpacity, Vibration, Share} from 'react-native';
+	import AsyncStorage from '@react-native-community/async-storage';
+	import styles from './Styles'
 	
 	
 	const instructions = Platform.select({
@@ -15,32 +17,74 @@ import React, {Component} from 'react';
 	    'Double tap R on your keyboard to reload,\n' +
 	    'Shake or press menu button for dev menu',
 	});
-	
+  
+  const INCREMENT = 1
 	export default class App extends Component {
-	
+  
+    startVibration(){
+      Vibration.vibrate()
+    }
+
 	  constructor (props) {
 	    super(props)
-	    this.state = {counter: 0}
+	    this.state = {counter: 1, result: ''}
 	    this.onResetPress = this.onResetPress.bind(this)
 	    this.onAddPress = this.onAddPress.bind(this)
 	    this.onSubstractPress = this.onSubstractPress.bind(this)
-	    this.increment = {increment: 10}
-	  }
+			this._shareMessage = this._shareMessage.bind(this)
+			this._showResult = this._showResult.bind(this)
+    }
+		
+		_showResult(result){
+			this.setState({result})
+		}
+
+		_shareMessage(){
+			Share.share({message: "My score! "}).then(this._showResult)
+		}
+
+    componentDidMount(){
+      this.getData()
+    }
 	
 	  onResetPress() {
-	    this.setState({counter: 0})
+      this.setState({counter: 0})
+      this.startVibration()
 	  }
 	
 	  onAddPress() {
-	    this.setState({counter: this.state.counter + this.increment.increment})
+      this.setState({counter: this.state.counter + INCREMENT})
+	    let number = INCREMENT
+      AsyncStorage.setItem('number', this.state.counter + INCREMENT)
+      this.startVibration()
 	  }
 	
-	  onSubstractPress() {
-	    this.setState({counter: this.state.counter - this.increment.increment})
-	    let number = this.increment.increment
-	    AsyncStorage.setItem = ('number', number)
-	  }
-	
+	   onSubstractPress() {
+	    this.setState({counter: this.state.counter - INCREMENT})
+	    let number = INCREMENT
+      AsyncStorage.setItem('number', this.state.counter - INCREMENT)
+      this.startVibration()
+    }
+
+    // storeData = async () => {
+    //   try {
+    //     await AsyncStorage.setItem('number', number)
+    //   } catch (e) {
+    //     // saving error
+    //   }
+    // }
+
+    getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('number')
+        if(value !== null) {
+        this.setState({counter: value})
+        }
+      } catch(e) {
+        // error reading value
+      }
+    }
+    
 	  render() {
 	    return (
 	    <View style = {styles.view}>
@@ -48,78 +92,68 @@ import React, {Component} from 'react';
 	      {this.state.counter}
 	      </Text>
 	
-	      <TouchableOpacity onPress = {this.onResetPress}>
+	      {/* <TouchableOpacity onPress = {this.onResetPress}>
 	      <Text style={styles.reset} title= "Reset" >
 	        Reset
 	      </Text>
-	      </ TouchableOpacity>
+	      </ TouchableOpacity> */}
+
+				<View style={styles.reset}>
+				<Button 
+				title="Reset"
+        onPress = {this.onResetPress}
+        color = 'white'
+        />
+				</View>
+
 	
 	      <View style = {styles.subView}>
-	      <TouchableOpacity onPress = {this.onSubstractPress}>
+	      {/* <TouchableOpacity onPress = {this.onSubstractPress}>
 	      <Text style={styles.minus} title= "minus" >
 	      -
 	      </Text>
 	      </ TouchableOpacity>
-	
-	      <TouchableOpacity onPress = {this.onAddPress}>
+			 */}
+
+				<View style={styles.minus}>
+				<Button 
+				title="-"
+        onPress = {this.onSubstractPress}
+        color = 'white'
+        />
+				</View>
+
+				<View style={styles.plus}>
+				<Button 
+				title="+"
+        onPress = {this.onAddPress}
+        color = 'white'
+        />
+				</View>
+
+	      {/* <TouchableOpacity onPress = {this.onAddPress}>
 	      <Text style={styles.plus} title= "plus" >
 	      +
 	      </Text>
-	      </ TouchableOpacity>
+	      </ TouchableOpacity> */}
 	      </View>
+
+        <View style={styles.scoreShare}>
+        <Button 
+        title="Share your score!"
+        onPress = {this._shareMessage}
+        color = 'white'
+        />
+        </View>
+
+				<Text>
+				{JSON.stringify(this.state.result)}
+				</Text>
+
 	    </View>
 	
 	    );
 	  }
 	}
 	
-	const styles = StyleSheet.create ({
-	  view: {
-	    justifyContent: "center", 
-	    alignItems: "center",
-	    flex: 1,
-	    backgroundColor: '#232528',
-	  },
-	  subView: {
-	    flexDirection: 'row',
-	    justifyContent: "center", 
-	    alignItems: "center",
-	    backgroundColor: '#232528',
-	  },
-	  counter: {
-	    fontSize: 100,
-	    color: 'white',
-	    padding: 100,
-	    margin: 10
-	  },
-	  reset: {
-	    alignItems: "center",
-	    textAlign: 'center',
-	    backgroundColor: '#5B8930',
-	    fontSize: 30,
-	    borderRadius: 10,
-	    overflow: 'hidden',
-	    color: 'white',
-	    padding: 10,
-	    width: 365,
-	    margin: 5
-	  },
-	  plus: {
-	    backgroundColor: '#8DB255',
-	    fontSize: 50,
-	    color: 'white',
-	    borderRadius: 10,
-	    overflow: 'hidden',
-	    padding: 75,
-	    margin: 5
-	  },
-	  minus: {
-	    backgroundColor: '#8DB255',
-	    fontSize: 50,
-	    color: 'white',
-	    borderRadius: 10,
-	    overflow: 'hidden',
-	    padding: 75,
-	    margin: 5
-	  }
-	}) 
+
