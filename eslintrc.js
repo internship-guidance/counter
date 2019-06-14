a4cedf204374e5d8c3caa37a273b3fc0e0590ab9
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-06-14 15:42:08 +0300

    added aditional libraries

---
+++ eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": ["airbnb", "prettier", "plugin:prettier/recommended"],
+  "rules": {
+    "prettier/prettier": "error",
+  "parser": "babel-eslint",
+  'env': {
+    "jest": true,
+  }
+}
