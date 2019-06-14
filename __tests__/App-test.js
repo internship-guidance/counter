71dba26bddd85b9010810bbea83306981c72adc5
Aris Uzars <aris.uzars@accenture.com>
2019-06-14 19:39:58 +0300

    Add pre-commit
    Pre-commit will run eslint and jest before each commit

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,10 +1,8 @@
-/**
- * @format
- */
+/* eslint-disable import/order */
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
